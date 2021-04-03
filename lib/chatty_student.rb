13c3ab4ee9157b3b4927c9331a29693f79cf6b49
Evelyn Kosma <evelyn.a.kosma@gmail.com>
2021-04-03 19:02:17 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,8 +1,12 @@
 class ChattyStudent < Student
   
   def hello
+<<<<<<< HEAD
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+=======
+    return super && "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+>>>>>>> 29623d946ecf02f9e35ad09baae98ab3f01c1147
   end
   
   def raise_hand
